c99bf8fb96a0f73bad9115f4a86f875403839c0c
Tim Kainz <tk144@hdm-stuttgart.de>
2021-11-30 23:37:18 +0100

    navigation wurde hinzugefuegt aber noch nicht eingebaut

--- app/src/main/java/com/example/thebeast/EinstellungenFragment.java
+++ app/src/main/java/com/example/thebeast/EinstellungenFragment.java
@@ -7,8 +7,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
+
+import com.example.thebeast.R;
 
 public class EinstellungenFragment extends Fragment {
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -16,5 +20,7 @@ public class EinstellungenFragment extends Fragment {
         return (ViewGroup) inflater.inflate(
                 R.layout.fragment_einstellungen, container, false);
     }
+
+
 
 }
--- app/src/main/java/com/example/thebeast/HomeFragment.java
+++ app/src/main/java/com/example/thebeast/HomeFragment.java
@@ -9,17 +9,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.HorizontalScrollView;
 
+import com.example.thebeast.R;
+
 
 public class HomeFragment extends Fragment {
 
 
-    HorizontalScrollView uebungen;
+    HorizontalScrollView trainingScrollview;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+
+
 
     }
 }
