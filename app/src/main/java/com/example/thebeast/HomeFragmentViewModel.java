1255551f46f1f7d87dfadeb47332d4193dbc7d59
Tim Kainz <tk144@hdm-stuttgart.de>
2022-01-30 18:25:36 +0100

    HomeFragmentViewModel hinzugefuegt mit gewaehlteTrainings Array

---
+++ app/src/main/java/com/example/thebeast/HomeFragmentViewModel.java
@@ -0,0 +1,12 @@
+package com.example.thebeast;
+
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+
+public class HomeFragmentViewModel extends ViewModel {
+
+    private ArrayList<Integer> gewaehlteTrainingsList = new ArrayList();
+    private ArrayList<String> gewaehlteTrainingsName = new ArrayList();
+
+}
