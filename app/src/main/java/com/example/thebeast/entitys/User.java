8b3418c3721ec5c79cdfff3fe68bef7a15420130
Tim Kainz <tk144@hdm-stuttgart.de>
2022-02-03 18:23:04 +0100

    Dependencies fuer android Architektur hinzugefuegt. Erste Entitys erstellt. User und Workout

---
+++ app/src/main/java/com/example/thebeast/entitys/User.java
@@ -0,0 +1,19 @@
+package com.example.thebeast.entitys;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity (tableName = "user_table")
+public class User {
+
+    @PrimaryKey(autoGenerate = true)
+    private String id;
+
+    private String beastName;
+
+    private String email;
+
+    private int wokoutlaenge;
+
+
+}
---
+++ app/src/main/java/com/example/thebeast/entitys/Workout.java
@@ -0,0 +1,4 @@
+package com.example.thebeast.entitys;
+
+public class Workout {
+}
