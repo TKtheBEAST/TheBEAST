0dac9d2786d0c9c19d0bc35a46a366580682fd5b
Tim Kainz <tk144@hdm-stuttgart.de>
2022-03-02 19:21:47 +0100

    Einstellungen workoutlaenge geprüft - bereits gewaehlte länge wird markiert. On click ergänzt -> Backend muss noch geschrieben werden

--- app/src/main/java/com/example/thebeast/EinstellungenFragment.java
+++ app/src/main/java/com/example/thebeast/EinstellungenFragment.java
@@ -1,10 +1,12 @@
 package com.example.thebeast;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.nfc.Tag;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,17 +14,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.HorizontalScrollView;
+import android.widget.Toast;
 
 import com.example.thebeast.R;
 import com.example.thebeast.businessobjects.UserModel;
+import com.example.thebeast.viewmodel.EinstellungenFragmentViewModel;
+import com.example.thebeast.viewmodel.HomeFragmentViewModel;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import static android.content.ContentValues.TAG;
 
-public class EinstellungenFragment extends Fragment {
+public class EinstellungenFragment extends Fragment implements View.OnClickListener {
 
-    Button abmeldenButton;
+    Button abmeldenButton,dreißigMinButton,eineHbutton,eineHdreißigButton,zweiHbutton;
+    private float aktuelleWorkoutlaenge;
+
+
+    EinstellungenFragmentViewModel einstellungenFragmentViewModel;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,6 +39,18 @@ public class EinstellungenFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_einstellungen, container, false);
 
         abmeldenButton = view.findViewById(R.id.abmeldenButton);
+        dreißigMinButton = view.findViewById(R.id.trainingslaenge30minButton);
+        eineHbutton = view.findViewById(R.id.trainingslaenge1hButton);
+        eineHdreißigButton = view.findViewById(R.id.trainingslaenge1_5hButton);
+        zweiHbutton = view.findViewById(R.id.trainingslaenge2hButton);
+
+        aktuelleWorkoutlaenge = CurrentUser.getCurrentUser().getWorkoutlaenge();
+        setFocusAktuelleLaenge(aktuelleWorkoutlaenge);
+
+
+        //initialisieren ViewModel
+        einstellungenFragmentViewModel = new ViewModelProvider(getActivity()).get(EinstellungenFragmentViewModel.class);
+
 
         abmeldenButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,5 +64,49 @@ public class EinstellungenFragment extends Fragment {
     }
 
 
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.trainingslaenge30minButton:
+                if(aktuelleWorkoutlaenge == 0.5){
+                    Toast.makeText(getActivity(),"Diese Workoutlänge ist bereits hinterlegt",Toast.LENGTH_LONG).show();
+                }else{
+                    einstellungenFragmentViewModel.updateWorkoutlaenge(0.5f);
+                }
+            case R.id.trainingslaenge1hButton:
+                if(aktuelleWorkoutlaenge == 1){
+                    Toast.makeText(getActivity(),"Diese Workoutlänge ist bereits hinterlegt",Toast.LENGTH_LONG).show();
+                }else {
+                    einstellungenFragmentViewModel.updateWorkoutlaenge(1f);
+                }
+            case R.id.trainingslaenge1_5hButton:
+                if(aktuelleWorkoutlaenge == 1.5){
+                    Toast.makeText(getActivity(),"Diese Workoutlänge ist bereits hinterlegt",Toast.LENGTH_LONG).show();
+                }else {
+                    einstellungenFragmentViewModel.updateWorkoutlaenge(1.5f);
+                }
+            case R.id.trainingslaenge2hButton:
+                if(aktuelleWorkoutlaenge == 2){
+                    Toast.makeText(getActivity(),"Diese Workoutlänge ist bereits hinterlegt",Toast.LENGTH_LONG).show();
+                }else {
+                    einstellungenFragmentViewModel.updateWorkoutlaenge(2f);
+                }
+        }
+    }
 
+    public void setFocusAktuelleLaenge(float aktuelleWorkoutlaenge){
+        if(aktuelleWorkoutlaenge == 0.5){
+            dreißigMinButton.setBackgroundColor(Color.RED);
+        }
+        if(aktuelleWorkoutlaenge == 1){
+            eineHbutton.setBackgroundColor(Color.RED);
+        }
+        if(aktuelleWorkoutlaenge == 1.5){
+            eineHdreißigButton.setBackgroundColor(Color.RED);
+        }
+        if(aktuelleWorkoutlaenge == 2){
+            zweiHbutton.setBackgroundColor(Color.RED);
+        }
+
+    }
 }
---
+++ app/src/main/java/com/example/thebeast/viewmodel/EinstellungenFragmentViewModel.java
@@ -0,0 +1,12 @@
+package com.example.thebeast.viewmodel;
+
+import androidx.lifecycle.ViewModel;
+
+public class EinstellungenFragmentViewModel extends ViewModel {
+
+    public EinstellungenFragmentViewModel(){}
+
+    public void updateWorkoutlaenge(float laenge){
+
+    }
+}
